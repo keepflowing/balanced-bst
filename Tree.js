35015f2bb03be87b04ddd31c3033ab841413c9c8
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-10 18:32:37 -0500

    Write buildTree function

--- Tree.js
+++ Tree.js
@@ -10,4 +10,23 @@ export default class Tree {
   constructor(arr) {
     this.arr = arr;
   }
+
+  /**
+   * @param {array} arr
+   * @param {int} start
+   * @param {int} end
+   * @return {Node}
+   */
+  buildTree(arr = this.arr, start = 0, end = arr.length - 1) {
+    if (start > end) {
+      return null;
+    }
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(
+        arr[mid],
+        this.buildTree(arr, start, mid - 1),
+        this.buildTree(arr, mid + 1, end),
+    );
+    return node;
+  }
 }

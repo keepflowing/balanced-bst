bde520e4fc2256e5929ded33f4c9e2c81c705e1e
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-10 18:32:53 -0500

    Write basic test

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import Tree from './Tree.js';
+import prettyPrint from './prettyPrint.js';
 
 const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(sortedArr);
+const tree = new Tree(sortedArr);
+
+const firstNode = tree.buildTree();
+
+
+prettyPrint(firstNode);

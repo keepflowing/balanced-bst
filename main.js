e944e68e088218dd22340415c147ba13fb672f76
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-13 13:41:19 -0500

    Write tests for pre- and postorder

--- main.js
+++ main.js
@@ -5,11 +5,20 @@ const sortedArr = [4, 8, 15, 16, 23, 42];
 const tree = new Tree(sortedArr);
 
 const firstNode = tree.buildTree();
-Tree.insert(2, firstNode);
-Tree.insert(1, firstNode);
-Tree.insert(3, firstNode);
 prettyPrint(firstNode);
+
 // console.log(Tree.find(23, firstNode));
-console.log(Tree.levelOrder(firstNode, (node) => {
+/* console.log(Tree.levelOrder(firstNode, (node) => {
   console.log(node.data);
-}));
+}));*/
+
+console.log(Tree.preorder(firstNode));
+
+Tree.preorder(firstNode, (node) => {
+  console.log(node.data);
+});
+
+console.log(Tree.postorder(firstNode));
+Tree.postorder(firstNode, (node) => {
+  console.log(node.data);
+});

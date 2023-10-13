8f2c2a88850e58947377c31edba3bf665285ecd4
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-13 14:22:06 -0500

    Write test for isBalanced and rebalance

--- main.js
+++ main.js
@@ -4,8 +4,17 @@ import prettyPrint from './prettyPrint.js';
 const sortedArr = [4, 8, 15, 16, 23, 42];
 const tree = new Tree(sortedArr);
 
-const firstNode = tree.buildTree();
+let firstNode = tree.buildTree();
 prettyPrint(firstNode);
+console.log(Tree.isBalanced(firstNode));
 
 // console.log(Tree.height(firstNode));
-console.log(Tree.depth(Tree.find(42, firstNode), firstNode));
+// console.log(Tree.depth(Tree.find(42, firstNode), firstNode));
+Tree.insert(43, firstNode);
+Tree.insert(55, firstNode);
+prettyPrint(firstNode);
+console.log(Tree.isBalanced(firstNode));
+
+firstNode = tree.rebalance(firstNode);
+prettyPrint(firstNode);
+console.log(Tree.isBalanced(firstNode));
